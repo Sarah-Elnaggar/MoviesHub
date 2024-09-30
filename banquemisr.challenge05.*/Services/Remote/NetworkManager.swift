269b310e156c635d8c146badd4f19e3b61ea1db8
Sara Elnaggar <sara.elnaggar.114@gmail.com>
2024-09-30 18:33:46 +0300

    Creating Network Manager and enum for errors

---
+++ banquemisr.challenge05.*/Services/Remote/NetworkManager.swift
@@ -0,0 +1,48 @@
+//
+//  NetworkManager.swift
+//  banquemisr.challenge05.*
+//
+//  Created by Sarah on 30/09/2024.
+//
+
+import Foundation
+
+protocol NetworkProtocol {
+    func fetch<T: Codable> (url: String, type: T.Type, completion: @escaping (T?, Error?) -> Void)
+}
+
+
+class NetworkManager: NetworkProtocol {
+    
+    func fetch<T: Codable> (url: String, type: T.Type, completion: @escaping (T?, Error?) -> Void) {
+        
+        guard let url = URL(string: url) else {
+            completion(nil, NetworkError.invalidURL)
+            return
+        }
+        print("Fetching data from URL: \(url)")
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(nil, NetworkError.other(error))
+                return
+            }
+            guard let data = data else {
+                completion(nil, NetworkError.noData)
+                return
+            }
+            if let httpResponse = response as? HTTPURLResponse, !(200...299).contains(httpResponse.statusCode) {
+                completion(nil, NetworkError.httpError(httpResponse.statusCode))
+                return
+            }
+            do {
+                let result = try JSONDecoder().decode(T.self, from: data)
+                completion(result, nil)
+            } catch {
+                completion(nil, NetworkError.decodingError(error.localizedDescription))
+            }
+        }
+        task.resume()
+    }
+}
+
