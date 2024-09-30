2089025c0c45b118eed2934c3e9be5d76486009b
Sara Elnaggar <sara.elnaggar.114@gmail.com>
2024-09-30 17:20:26 +0300

    Creating API Manager

--- banquemisr.challenge05.*/Services/Remote/APIManager.swift
+++ banquemisr.challenge05.*/Services/Remote/APIManager.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+protocol APIManagerProtocol {
+    func getUrl(for endpoint: EndPoint) -> String
+}
+
+
+class APIManager: APIManagerProtocol {
+    
+    private let baseURL = "https://api.themoviedb.org/3/movie/"
+    private let apiKey = "6974796546c7f41cece685a979189536"
+    
+
+    func getUrl(for endpoint: EndPoint) -> String {
+        
+        let path =  endpoint.path
+        let url = "\(baseURL)\(path)?api_key=\(apiKey)"
+        
+        return url
+    }
+}
